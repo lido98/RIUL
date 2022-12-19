71affae10eb268f17eb5443e5d5c0be452020901
Raúl Beltrán <rb58853@gmail.com>
2022-12-19 23:22:19 +0100

    Latent semantic with correct weigth

--- Backend/data/Models/Latent_Semantic_Indexing_Model/latent_semantic_search_engine.py
+++ Backend/data/Models/Latent_Semantic_Indexing_Model/latent_semantic_search_engine.py
@@ -56,29 +56,83 @@ class LatentSemanticMatrix:
                 wij = gi * log10(self.tf(word,document)+1)
                 self.matrix[self.index_document[document]][i] = wij
     
-    def gi(self,word):
-        gf = self.trie.get_total_count(word)
+    def gi(self,word, q = 0,f = 0):
+        '''f is freqs by word in query, q is 1 if gi is used for query'''
+        gf = self.trie.get_total_count(word) + q
         sum = 0
         for document in self.trie.documents_of_word(word):
             tf = self.tf(word,document)
             value =self.pij(tf,gf) 
             sum += (value*log10(value))
-        return 1 + sum/log10(self.trie.total_documents)
+        tf = f
+        value =self.pij(tf,gf) 
+        sum += (value*log10(value))
+        return 1 + sum/log10(self.trie.total_documents+q)
 
     def pij(self, tf,gf): return tf/gf
 
     def tf(self,word,document):    
-        n = self.trie.word_count_in_document(word,document)    
+        return self.trie.word_count_in_document(word,document)    
         d = self.trie.max_count_in_document[document]
         return n/d
 
+
     def freqs_in_query(self, query):
         result = {}
-        max_count = 1
         for term in query.split(" "):
             try: 
                 result[term] +=1
                 max_count = max(max_count,result[term])
             except: result [term] = 1
             
-        return [result,max_count]    
+        return result
+
+    def sim_list(self, query):
+        freqs = self.freqs_in_query(query)
+        
+        vector_query = []
+        N = self.total_documents
+
+        query_list_index_value = []
+        by_norm_query = []
+        for word in freqs:
+            try:
+                lij = log10(freqs[word]+1)
+                gi = self.gi(word,1,freqs[word])
+                by_norm_query.append(lij*gi)
+                word_index_value = self.index_word[word]
+                query_list_index_value.append(word_index_value)
+                vector_query.append(lij*gi)
+            except: pass
+
+        rank = {}
+        
+        norm_query = np.linalg.norm(by_norm_query)
+        new_matrix = []
+        for i in range(self.total_documents):
+            new_matrix.append([])
+            for index in query_list_index_value:
+                value = self.matrix[i][index]
+                new_matrix[i].append(value)
+
+    
+        for document,d in zip(new_matrix, range(len(self.matrix))):
+            doc = self.trie.documents[d]
+            norm_doc = self.norms[doc]
+                
+            rank[doc]= self.sim(document, vector_query,norm_doc, norm_query) 
+        return rank
+
+    def sim (self, doc, query, norm_doc, norm_query):
+        num = np.dot(doc, query)
+        sim = num/(norm_doc*norm_query)
+        return sim
+    
+    def full_norms(self):
+        for doc,vector in zip(self.trie.documents, self.matrix):
+            self.norms[doc] = np.linalg.norm(vector)
+
+    def get_rank_of_query(self,query, a):
+        result = sorted(self.sim_list(query,a).items(), key=lambda item: item[1])
+        result = dict(reversed(list(result)))
+        return result
