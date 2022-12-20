dda943c1c8b7d37a9e684e8dfc4d3cd6db85c9df
lido98 <lidierrc@gmail.com>
2022-12-20 18:55:17 +0100

    stop words deleted

--- Backend/data/indexer.py
+++ Backend/data/indexer.py
@@ -11,30 +11,26 @@ class InvertedIndex:
         self.trie = Trie(root=True)
 class Indexer: 
     def __init__(self, docs: Collection):
-        self.docs = docs.doc_bodies()
         t0 = time.time()
-        self.docs = self.select_keywords(self.docs)  
+        self.docs = self.select_keywords(docs)  
         t = time.time() - t0
         a = 2
-
     
     def __call__(self) -> InvertedIndex:
         index = InvertedIndex()
         index.trie(self.docs)
         return index
     
-    def select_keywords(self, docs) -> Collection:
+    def select_keywords(self, docs: Collection) -> Collection:
         stop_words = set(stopwords.words('english'))
         new_docs = []
         for doc in docs.__iter__():
-            tokenized_body = word_tokenize(doc.lower())
+            tokenized_body = word_tokenize(doc.body.lower())
             tokenized_body = [WordNetLemmatizer().lemmatize(w) for w in tokenized_body]
             tokenized_body = [token for token in tokenized_body if not token in stop_words]
-            new_docs.append(tokenized_body)
-            #new_doc = Document(doc.id, doc.title, " ".join(tokenized_body))
-            #new_docs.append(new_doc)
-        #return Collection(new_docs)
-        return new_docs
+            new_doc = Document(doc.id, doc.title, " ".join(tokenized_body))
+            new_docs.append(new_doc)
+        return Collection(new_docs)
     
     def select_nouns(self, docs: Collection):
         #stop_words = set(stopwords.words('english'))
@@ -45,7 +41,7 @@ class Indexer:
             tokenized_body = [WordNetLemmatizer().lemmatize(w) for w in tokenized_body]            
             tokenized_body = [token for (token, pos) in pos_tag(tokenized_body) if is_noun(pos)]
              
-            new_doc = Document(doc.id, doc.title, tokenized_body)
+            new_doc = Document(doc.id, doc.title, " ".join(tokenized_body))
             new_docs.append(new_doc)
         return Collection(new_docs)
     
